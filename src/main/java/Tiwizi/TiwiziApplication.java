5be3b2a4832d523bfa278995b83bc4c7c9784ce7
hajarwalfi <wh.hajarwalfi@gmail.com>
2025-12-21 13:48:27 +0100

    feat(setup) : TIWIZI-12 - initialize project with Maven wrapper and Spring Boot setup

---
+++ src/main/java/Tiwizi/TiwiziApplication.java
@@ -0,0 +1,13 @@
+package Tiwizi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiwiziApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TiwiziApplication.class, args);
+    }
+
+}
---
+++ src/test/java/Tiwizi/TiwiziApplicationTests.java
@@ -0,0 +1,13 @@
+package Tiwizi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiwiziApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
