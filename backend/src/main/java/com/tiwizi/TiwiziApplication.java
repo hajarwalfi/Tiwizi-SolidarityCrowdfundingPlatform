5e09663457f030827bf1ff654512b04d59c5d323
hajarwalfi <wh.hajarwalfi@gmail.com>
2025-12-22 11:58:07 +0100

    feat(frontend):TIWIZI-34 - initialize Angular application with basic structure and configuration files

--- backend/src/main/java/com/tiwizi/TiwiziApplication.java
+++ backend/src/main/java/com/tiwizi/TiwiziApplication.java
@@ -5,9 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TiwiziApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(TiwiziApplication.class, args);
-    }
+    public static void main(String[] args) {SpringApplication.run(TiwiziApplication.class, args);}
 
 }
