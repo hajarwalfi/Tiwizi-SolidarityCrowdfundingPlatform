342b0090ba60bfddd80bdc87ede9ae2bdf325c7d
hajarwalfi <wh.hajarwalfi@gmail.com>
2025-12-23 10:04:28 +0100

    feat(tailwind): TIWIZI-37 - add Tailwind CSS test component and update routing

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { TailwindTest } from './shared/components/tailwind-test/tailwind-test';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: TailwindTest
+  },
+  {
+    path: 'tailwind-test',
+    component: TailwindTest
+  }
+];
